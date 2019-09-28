debf77b9889426ff5b77c52948dc2ffbb50e342e
Christophe Thibaut <cthibaut@octo.com>
2019-09-28 11:04:05 +0200

    hcat and vcat

--- First/DiagramsTutorial.hs
+++ First/DiagramsTutorial.hs
@@ -8,5 +8,11 @@ import Diagrams.Backend.SVG.CmdLine
 myCircle :: Diagram B
 myCircle = circle 1 # fc red # lw none ||| circle 1 # fc green # lw none ||| (square 1 # fc aqua `atop` circle 1 # showOrigin) === (square 2 # fc purple)
 
-main = mainWith myCircle
+circles :: Diagram B
+circles = hcat (map circle [1..6])
+
+example :: Diagram B
+example = vcat (replicate 3 circles)
+
+main = mainWith example
 
