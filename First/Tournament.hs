b421355137bd5cf082710496fb279f0edb04ab27
Christophe Thibaut <cthibaut@octo.com>
2019-09-28 15:08:37 +0200

    tournament : connect all nodes with applyAll

--- First/Tournament.hs
+++ First/Tournament.hs
@@ -10,10 +10,10 @@ node :: Int -> Diagram B
 node n = text (show n) # fontSizeL 0.2 # fc white <> circle 0.2 # fc green # named n
 
 tournament :: Int -> Diagram B 
-tournament n = atPoints (trailVertices $ regPoly n 1) (map node [1..n])
+tournament n = atPoints (trailVertices $ regPoly n 1) (map node [1..n]) # applyAll [connectOutside' arrowOpts j k | j <- [1..n-1], k <- [j+1..n]]
 
 
+arrowOpts = with & gaps .~ small
+                 & headLength .~ local 0.15
 
-main = mainWith $ tournament 6 # connectOutside' (with & gaps .~ small
-                                                       & headLength .~ local 0.15)
-    (1 :: Int) (2 :: Int)
+main = mainWith $ tournament 6 
