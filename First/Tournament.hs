042571ce3b1acb68042a603634bcf4811eacf57e
Christophe Thibaut <cthibaut@octo.com>
2019-09-28 15:02:46 +0200

    tournament : name nodes and connect 2 of them

--- First/Tournament.hs
+++ First/Tournament.hs
@@ -7,11 +7,11 @@ import Diagrams.Backend.SVG.CmdLine
 
 
 node :: Int -> Diagram B
-node n = text (show n) # fontSizeL 0.2 # fc white <> circle 0.2 # fc green
+node n = text (show n) # fontSizeL 0.2 # fc white <> circle 0.2 # fc green # named n
 
 tournament :: Int -> Diagram B 
 tournament n = atPoints (trailVertices $ regPoly n 1) (map node [1..n])
 
 
 
-main = mainWith $ tournament 6
+main = mainWith $ tournament 6 # connectOutside (1 :: Int) (2 :: Int)
