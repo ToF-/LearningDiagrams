ae441829c20c6eba86d239e5718877998574436a
Christophe Thibaut <cthibaut@octo.com>
2019-09-28 11:01:21 +0200

    juxtaposing vertically

--- First/DiagramsTutorial.hs
+++ First/DiagramsTutorial.hs
@@ -6,7 +6,7 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
 myCircle :: Diagram B
-myCircle = circle 1 # fc red # lw none ||| circle 1 # fc green # lw none ||| (square 1 # fc aqua `atop` circle 1 # showOrigin)
+myCircle = circle 1 # fc red # lw none ||| circle 1 # fc green # lw none ||| (square 1 # fc aqua `atop` circle 1 # showOrigin) === (square 2 # fc purple)
 
 main = mainWith myCircle
 
