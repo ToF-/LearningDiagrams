a7d3624128740ee400d8e68dd665f1ee18f91a02
Christophe Thibaut <cthibaut@octo.com>
2019-09-28 11:07:48 +0200

    beside and vector juxtaposition

--- First/DiagramsTutorial.hs
+++ First/DiagramsTutorial.hs
@@ -14,5 +14,11 @@ circles = hcat (map circle [1..6])
 example :: Diagram B
 example = vcat (replicate 3 circles)
 
-main = mainWith example
+circleSqV1 :: Diagram B
+circleSqV1 = beside (r2 (1,1)) (circle 1) (square 2)
+
+circleSqV2 :: Diagram B
+circleSqV2 = beside (r2 (1,-0.2)) (circle 2) (square 1)
+
+main = mainWith $ hcat [circleSqV1, strutX 1, circleSqV2]
 
