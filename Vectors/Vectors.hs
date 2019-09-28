958f8c7fb10c538c024d28c179bdf2bffb73ab70
Christophe Thibaut <cthibaut@octo.com>
2019-09-28 21:50:54 +0200

    units and vectors, r2

--- Vectors/Vectors.hs
+++ Vectors/Vectors.hs
@@ -6,6 +6,6 @@ import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
 
 example :: Diagram B
-example = fromOffsets [unitX, unitY, 2*^ unit_X, unit_Y] # centerXY
+example = fromOffsets . map r2 $ [(1,1), (0,3), (-2,1), (-1,-4)]
 
 main = mainWith example
