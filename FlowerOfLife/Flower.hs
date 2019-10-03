e5dd488202487621739306d1a834914eb2e7fbf1
Christophe Thibaut <cthibaut@octo.com>
2019-10-03 08:31:49 +0200

    one flower of life (every 2pi/5, draw an arc of 3*pi/5)

--- FlowerOfLife/Flower.hs
+++ FlowerOfLife/Flower.hs
@@ -8,13 +8,13 @@ import Diagrams.TwoD.Vector
 import Diagrams.TwoD.Arc
 
 flower :: Diagram B
-flower = atPoints (fromOffsets pts) arcs
+flower = atPoints (fromOffsets pts) (take 5 arccs)
     where
     pts :: [V2 Double]
     pts = [1 *^ e (a @@rad) | a <- [0,tau/5..tau]]
     arcs :: [Diagram B]
-    arcs= [arc (direction (1 *^ e (d @@ rad))) (a @@ rad) #showOrigin  
-          | d <- [tau/5..tau], let a = (tau)]
-
+    arcs= [arc (direction (1 *^ e (d @@ rad))) ((3*pi/5) @@ rad) 
+          | d <- [0,tau/5..]]
+    arccs = zipWith (\a c -> a # lc c) arcs [black,purple,green,blue,red,orange]
 
 main = mainWith $ flower
