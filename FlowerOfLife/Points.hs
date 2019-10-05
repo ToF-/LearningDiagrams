375fc16541f08ee385043a1631ffc32b0cd340b8
Christophe Thibaut <cthibaut@octo.com>
2019-10-05 12:12:55 +0200

    reflectAbout

--- FlowerOfLife/Points.hs
+++ FlowerOfLife/Points.hs
@@ -7,15 +7,23 @@ import Diagrams.Backend.SVG.CmdLine
 import Diagrams.TwoD.Vector
 
 diagram :: Diagram B
-diagram = atPoints pts (repeat (circle 0.05 # lw none # fc black))
+diagram = penta pts0 black
+   `atop` nextPoint (penta pts0 blue)
     where
-    r :: V2 Double
-    r = unitY
+    penta pts c = atPoints pts (map (\n ->  (text (show n) # fontSizeL 0.05 # fc white <> circle 0.05 # lw none # fc c)) [0..4]) # showOrigin
+    nextPoint = reflectAbout (d ^& d) (rotateBy (fromIntegral 1 / 5) xDir)
+    a = 0
+    d = 1 
 
-    pts :: [P2 Double]
-    pts = trailVertices (pentagon 1)
+pts0 :: [P2 Double]
+pts0 = trailVertices (pentagon 1)
 
-main = mainWith $ diagram
+pts1 :: [P2 Double]
+pts1 = trailVertices (pentagon 1) # translateX t  # rotate (a@@rad) 
+    where
+    t = 1
+    a = -pi/5
+main = mainWith $ diagram 
 
 
 -- import Diagrams.Trail  -- for trailPoints
@@ -28,3 +36,5 @@ main = mainWith $ diagram
 --  , circle 1 `beneath` visPoints (trailPoints (circle 1))
 --  , hexagon 1 `beneath` visPoints (trailVertices (hexagon 1))
 --  ]
+--  reflectAbout (0 ^& 0) (rotateBy (- fromIntegral i / 6) xDir)
+--
