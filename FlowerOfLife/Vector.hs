bb1c2b9f503eb5f5ae40e8ae54f22319262f2f60
Christophe Thibaut <cthibaut@octo.com>
2019-10-05 14:37:28 +0200

    scaleRotateTo

--- FlowerOfLife/Vector.hs
+++ FlowerOfLife/Vector.hs
@@ -7,13 +7,25 @@ import Diagrams.Backend.SVG.CmdLine
 import Diagrams.TwoD.Vector
 
 diagram :: Diagram B
-diagram = mempty
+diagram = pentagon 1 # lc gray 
+      `atop` fromOffsets [vcs!!0] # lc purple # dashingG [0.01,0.01] 0
+      `atop` pentagon 1 # scaleRotateo (vcs!!0) # lc gray
+      `atop` fromOffsets [vcs!!1] # lc green # dashingG [0.01,0.01] 0
+      `atop` fromOffsets [vcs!!2] # lc blue # dashingG [0.01,0.01] 0
+      `atop` fromOffsets [vcs!!3] # lc red # dashingG [0.01,0.01] 0
+      `atop` fromOffsets [vcs!!4] # lc orange # dashingG [0.01,0.01] 0
+      
     where
-    r :: V2 Double
-    r = unitY
+    vts = trailVertices (pentagon 1)
+    vcs = zipWith (\p q -> (fromPoint p) ^+^ (fromPoint q)) vts (tail vts ++ [vts!!0])
+    fromPoint = r2 . unp2
+    v  = (r2.unp2) (vts!!0)
+    w  = (r2.unp2) (vts!!1)
+    vw = v ^+^ w
+    x  = (r2.unp2) (vts!!2)
+    y  = (r2.unp2) (vts!!3)
+    z  = (r2.unp2) (vts!!4)
 
-    p :: Path V2 Double
-    p = atPoints r
 
 main = mainWith $ diagram
 
